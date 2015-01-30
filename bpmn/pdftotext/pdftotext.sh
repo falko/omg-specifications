46e97adbc94388db576e2eb36da04468e260fd3e
Falko Menge <falko.menge@camunda.com>
2015-01-30 14:27:56 +0100

    Add chapters of BPMN 2.0.2 (formal-13-12-09)

--- bpmn/pdftotext/pdftotext.sh
+++ bpmn/pdftotext/pdftotext.sh
@@ -2,11 +2,11 @@
 #pdftotext -nopgbrk ../*.pdf bpmn.txt
 pdftotext        -l  30 -nopgbrk ../*.pdf bpmn-chapter-00.txt
 pdftotext -f  31 -l  41 -nopgbrk ../*.pdf bpmn-chapter-01-02.txt
-pdftotext -f  42 -l  50 -nopgbrk ../*.pdf bpmn-chapter-03-06.txt
-pdftotext -f  51 -l  78 -nopgbrk ../*.pdf bpmn-chapter-07.txt
-pdftotext -f  79 -l 138 -nopgbrk ../*.pdf bpmn-chapter-08.txt
-pdftotext -f 139 -l 174 -nopgbrk ../*.pdf bpmn-chapter-09.txt
-pdftotext -f 175 -l 344 -nopgbrk ../*.pdf bpmn-chapter-10.txt
+pdftotext -f  42 -l  48 -nopgbrk ../*.pdf bpmn-chapter-03-06.txt
+pdftotext -f  49 -l  76 -nopgbrk ../*.pdf bpmn-chapter-07.txt
+pdftotext -f  77 -l 136 -nopgbrk ../*.pdf bpmn-chapter-08.txt
+pdftotext -f 137 -l 172 -nopgbrk ../*.pdf bpmn-chapter-09.txt
+pdftotext -f 173 -l 344 -nopgbrk ../*.pdf bpmn-chapter-10.txt
 pdftotext -f 345 -l 396 -nopgbrk ../*.pdf bpmn-chapter-11.txt
 pdftotext -f 397 -l 454 -nopgbrk ../*.pdf bpmn-chapter-12.txt
 pdftotext -f 455 -l 474 -nopgbrk ../*.pdf bpmn-chapter-13.txt
@@ -17,11 +17,11 @@ pdftotext -f 511 -l 528 -nopgbrk ../*.pdf bpmn-chapter-b.txt
 pdftotext -f 529        -nopgbrk ../*.pdf bpmn-chapter-c.txt
 pdftotext        -l  30 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-00.txt
 pdftotext -f  31 -l  41 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-01-02.txt
-pdftotext -f  42 -l  50 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-03-06.txt
-pdftotext -f  51 -l  78 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-07.txt
-pdftotext -f  79 -l 138 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-08.txt
-pdftotext -f 139 -l 174 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-09.txt
-pdftotext -f 175 -l 344 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-10.txt
+pdftotext -f  42 -l  48 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-03-06.txt
+pdftotext -f  49 -l  76 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-07.txt
+pdftotext -f  77 -l 136 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-08.txt
+pdftotext -f 137 -l 172 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-09.txt
+pdftotext -f 173 -l 344 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-10.txt
 pdftotext -f 345 -l 396 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-11.txt
 pdftotext -f 397 -l 454 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-12.txt
 pdftotext -f 455 -l 474 -layout -nopgbrk ../*.pdf bpmn-with-layout-chapter-13.txt
