772977166e0eeeff073eb2a68aa44acd78d42db1
Falko Menge <falko.menge@camunda.com>
2016-05-22 03:04:14 +0200

    DMN 1.1 Beta (dtc-15-11-51)

---
+++ dmn/pdftotext/pdftotext.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+pdftotext -nopgbrk ../*.pdf dmn.txt
+sed -n '
+# if the first line copy the pattern to the hold buffer
+1h
+# if not the first line then append the pattern to the hold buffer
+1!H
+# if the last line then ...
+$ {
+        # copy from the hold to the pattern buffer
+        g
+        # do the search and replace
+        # first remove left page footer to avoid conflicts with chapter numbers
+        s/\n[0-9]\+\n\nDecision Model and Notation 1.1 FTF Convenience Document\n//g
+        # then remove right page footer
+        s/\nDecision Model and Notation 1.1 FTF Convenience Document\n\n[0-9]\+\n//g
+        # print
+        p
+}
+' dmn.txt > dmn-without-footer.txt;
